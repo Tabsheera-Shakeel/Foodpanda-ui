c0e11020d1f011a9ad2ea678ed4221b885ccbb9a
Tabsheera Shakeel <tabsheerasha123@gmail.com>
2024-09-30 08:57:06 +0500

    Initial commit from Create Next App

---
+++ src/app/header.tsx
@@ -0,0 +1,22 @@
+function Header(){
+
+    return(
+        <header>
+            <div>
+        <h1>logo</h1>
+            </div>
+
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/Home"></Link>
+                    </li>
+                </ul>
+                <li>
+                    <Link href="/About Me"></Link>
+                </li>
+                <li>
+                    <Link href="/Get In touch"
+                </li>
+            </nav>
+        </heade
